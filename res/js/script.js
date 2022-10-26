fe39e501aac1cf7fb010fcff9ec262039182d6e6
emily <emilychealy4@gmail.com>
2022-10-26 15:18:18 +0100

    got the value on the input to be displayed

---
+++ res/js/script.js
@@ -0,0 +1,66 @@
+const one = document.getElementById("one").value;
+const two = document.getElementById("two").value;
+const three = document.getElementById("three").value;
+const four = document.getElementById("four").value;
+const five = document.getElementById("five").value;
+const six = document.getElementById("six").value;
+const seven = document.getElementById("seven").value;
+const eight = document.getElementById("eight").value;
+const nine = document.getElementById("nine").value;
+
+
+const handleDigitsValue = () =>{
+  document.getElementById("zero").innerHTML = one;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const one = document.getElementById("one").textContent;
+// const two = document.getElementById("two").textContent;
+// const three = document.getElementById("three").textContent;
+// const four = document.getElementById("four").textContent;
+// const five = document.getElementById("five").textContent;
+// const six = document.getElementById("six").textContent;
+// const seven = document.getElementById("seven").textContent;
+// const eight = document.getElementById("eight").textContent;
+// const nine = document.getElementById("nine").textContent;
+
+// const numberWrapOne = [0];
+// const numberWrapTwo = [];
+
+// console.log(one);
+
+// const getNodeValue = (id) => {
+//   let elem = document.getElementById(id).textContent;
+//   console.log(numberWrapOne.push(elem))
+// };
+
+// console.log();
